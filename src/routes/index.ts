1fba52a03f11a812dad50789cd408252b7c2e90a
Henri <henri.silvatj@hotmail.com>
2020-08-29 19:10:45 -0300

    Add / endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,14 @@ import usersRouter from './users.routes';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  response.json({
+    description: 'A Typescript API to show user posts',
+    status: 'Running',
+    version: '1.0.0',
+  });
+});
+
 routes.use('/users', usersRouter);
 
 export default routes;
